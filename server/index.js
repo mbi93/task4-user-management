d9e29fcbf257727189e60845d2b497e697ed1006
mbi93 <68715337+mbi93@users.noreply.github.com>
2026-06-14 14:09:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -359,7 +359,7 @@ app.delete("/users/unverified", authMiddleware, async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
